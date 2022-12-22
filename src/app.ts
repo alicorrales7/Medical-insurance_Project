759a5265e5b42ba2936faa936cca0dbcf325811e
Ali Corrales <alicorrales2013@gmail.com>
2022-12-21 21:27:01 -0500

    include cors

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 import { loadApiEndpoints } from "./controllers/api";
 import { fileRoute } from "./routes/fileRoutes";
+import cors from "cors"
 //import { connectDB } from "./util/connection";
 
 // Create Express server
@@ -13,6 +14,7 @@ const app = express();
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
